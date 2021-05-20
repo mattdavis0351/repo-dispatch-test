1a0eba680b7256c6fc6481c3350065d1c5d7778a
mattdavis0351 <mattdavis0351@github.com>
2021-05-20 13:05:49 -0700

    try truthy value for octokit

--- .github/actions/check-secrets-action/main.js
+++ .github/actions/check-secrets-action/main.js
@@ -6,7 +6,7 @@ async function run() {
   try {
     const token = core.getInput("your-secret");
     console.log("trying to get an octokit");
-    const octokit = github.getOctokit("");
+    const octokit = github.getOctokit("ksjaklfjaklsjfkldjsklajfkl");
     console.log("done getting octokit");
     const { owner, repo } = github.context.repo;
     const results = await gradeLearner(octokit, owner, repo);
